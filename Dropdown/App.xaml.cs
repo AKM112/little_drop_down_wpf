95f2cd85e464d59f95fbc27fcfd1567fc7e17956
akm112 <akm122122122@gmail.com>
2025-04-03 19:49:26 +0200

    init

---
+++ Dropdown/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Dropdown;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
